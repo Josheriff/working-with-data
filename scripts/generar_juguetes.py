425cbb9c096140b996b115e9228d0eb64723f704
JMGallego <jmgallego@ZiMac4-Master.local>
2019-11-20 21:16:00 +0100

    creados juguetes

--- scripts/generar_juguetes.py
+++ scripts/generar_juguetes.py
@@ -1,33 +1,33 @@
 from database import create_connection
 
-TABLA_JUGUETES = """CREATE TABLE juguetes (
-    juguete_id          INTEGER PRIMARY KEY,
-    nombre              varchar NOT NULL,
-    precio              INTEGER NOT NULL,
-    edad_recomendada    INTEGER NOT NULL,
-    popularidad         INTEGER NOT NULL
-    );"""
-
 primer_juguete = {'nombre': 'Oso Peluche',
  'precio':2, 'edad_recomendada': 1,
-  'popularidad':0, 'mostrar': True}
+  'popularidad':0}
 
 segundo_juguete = {'nombre': 'EscalesTrix',
- 'precio':60, 'edad_recomendada': 9, 'popularidad':9,
-  'mostrar': True}
+ 'precio':60, 'edad_recomendada': 9, 'popularidad':9}
 
 tercer_juguete = {'nombre': 'Patines',
  'precio':60, 'edad_recomendada': 12,
-  'popularidad':5, 'mostrar': True} 
+  'popularidad':5}
+
+juguetes = [primer_juguete, segundo_juguete, tercer_juguete] 
 
 conn = create_connection()
+def crear(data):
+    nombre = data.get('nombre')
+    precio = data.get('precio')
+    edad_recomendada = data.get('edad_recomendada')
+    popularidad = data.get('popularidad')
 
-def crear(conn):
     with conn:
-        sql = f''' INSERT INTO nenes(nombre, precio, edad_recomendad, popularidad)
-                VALUES("{nombre}", {edad}, {esta_registrado}, {karma}) '''
+        sql = f''' INSERT INTO juguetes(nombre, precio, edad_recomendada, popularidad)
+                VALUES("{nombre}", {precio}, {edad_recomendada}, {popularidad}) '''
         
         cur = conn.cursor()
         cur.execute(sql)
-        nene_id = cur.lastrowid
-        return nene_id
+        print(cur.lastrowid)
+        return cur.lastrowid
+
+for juguete in juguetes:
+    crear(juguete)
